f5860c9f2fd1184cba6a7693de8b8bde2054f431
TheoMitchell <theomitchell@gmail.com>
2022-04-23 07:40:21 -0400

    6.B. Adjust portfolio.js, some Sass for portfolio and a test to send email from catact form

--- scripts/portifolio.js
+++ scripts/portifolio.js
@@ -10,9 +10,12 @@ const btn2 = document.getElementById("project-2-btn");
 const btn3 = document.getElementById("project-3-btn");
 const btn4 = document.getElementById("project-4-btn");
 
+
 // Store the <span> element that closes the modal
-const close = document.getElementsByClassName("close")[0];
-const close2 = document.getElementById("close2");
+const close1 = document.getElementsByClassName("close")[0];
+const close2 = document.getElementsByClassName("close")[1];
+const close3 = document.getElementsByClassName("close")[2];
+const close4 = document.getElementsByClassName("close")[3];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
@@ -25,23 +28,41 @@ btn3.onclick = function() {
   modal3.style.display = "block";
 }
 btn4.onclick = function() {
+  console.log(btn4)
   modal4.style.display = "block";
 }
 
 
 
 // When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-  modal.style.display = "none";
+close1.onclick = function() {
+  modal1.style.display = "none";
 }
 close2.onclick = function() {
-  modal.style.display = "none";
+  modal2.style.display = "none";
 }
+close3.onclick = function() {
+  modal3.style.display = "none";
+}
+close4.onclick = function() {
+  modal4.style.display = "none";
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else {
+      if (event.target == modal2) {
+        modal2.style.display = "none"; 
+      } else {
+          if (event.target == modal3) {
+            modal3.style.display = "none"; 
+          } else {
+              modal4.style.display = "none";    
+            } 
+        }
+    }
 }
 
