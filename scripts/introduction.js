e778bf0f597d8ecdeacd72ea79a06795b8a1ccfb
TheoMitchell <theomitchell@gmail.com>
2022-05-16 12:08:09 -0400

    20. Adjustments in the display

--- scripts/introduction.js
+++ scripts/introduction.js
@@ -6,64 +6,61 @@ const srcImage5 = document.getElementById("thumbnail-image-5");
 const targetTextContainer = document.querySelector(".text-container");
 const introductionContainer = document.querySelector(".introduction");
 
-
 let h1Text = document.querySelector("h1");
 let h3Text = document.querySelector("h3");
 let aLink = document.getElementById("aLink");
 let h4Text = document.querySelector("h4");
 let pText = document.querySelector("p");
-console.log(aLink)
+console.log(aLink);
 
-srcImage2.addEventListener('click', () => {
-  targetImage.src = './assets/images/personal/2.JPEG';
+srcImage2.addEventListener("click", () => {
+  targetImage.src = "./assets/images/personal/2.JPEG";
 
-  h1Text.innerText = 'Student and Professional';
-  h3Text.innerText = 'Eager to learn and highly motivated.';
-  aLink.innerText =  '';
-  aLink.href =  '';
-  h4Text.innerText = '';
-  pText.classList.remove('italic');
-  pText.innerText = `Graduated from Centennial College with high honours and Juno College of Technology. My  proven skills, knowledge, and abilities in programming, debugging, and testing web or desktop-based application, makes me excellent asset for your organization.`;
-  console.log(aLink)
+  h1Text.innerText = "Student and Professional";
+  h3Text.innerText = "Eager to learn and highly motivated.";
+  aLink.innerText = "";
+  aLink.href = "";
+  h4Text.innerText = "";
+  pText.classList.remove("italic");
+  pText.innerText = `Graduated from Centennial College with high honours and Juno College of Technology. My  proven skills, knowledge, and abilities in programming, debugging, and testing web or desktop-based application, makes me an excellent asset to your organization.`;
+  console.log(aLink);
 });
 
-srcImage3.addEventListener('click', () => {
-  targetImage.src = './assets/images/personal/3.jpg';
+srcImage3.addEventListener("click", () => {
+  targetImage.src = "./assets/images/personal/3.jpg";
 
-  h1Text.innerText = 'Soft Skills';
-  h3Text.innerText = 'Writing code to change the world!';  
-  aLink.innerText =  '';
-  aLink.href =  '';
-  h4Text.innerText = '';
-  pText.classList.remove('italic');
-  pText.innerText = `Excellent communicator both in English and Portuguese, reliable, proactive, effective team player, efficient, committed, time and goal oriented and critical thinker. `
-
+  h1Text.innerText = "Soft Skills";
+  h3Text.innerText = "Writing code to change the world!";
+  aLink.innerText = "";
+  aLink.href = "";
+  h4Text.innerText = "";
+  pText.classList.remove("italic");
+  pText.innerText = `Excellent communicator both in English and Portuguese, reliable, proactive, effective team player, efficient, committed, time and goal-oriented and critical thinker. `;
 });
 
-srcImage4.addEventListener('click', () => {
-  targetImage.src = './assets/images/personal/4.jpg';
+srcImage4.addEventListener("click", () => {
+  targetImage.src = "./assets/images/personal/4.jpg";
 
-  
-  h1Text.innerText = 'Interview';
-  h3Text.innerText = '';   
+  h1Text.innerText = "Interview";
+  h3Text.innerText = "";
   aLink.innerText = "Link to Interview";
-  aLink.href = "https://wimtach.centennialcollege.ca/centennial-college-graduate-rediscovers-his-passion-for-it/";
-  h4Text.innerText = '';
-  pText.classList.remove('italic');
-  pText.innerText = `As an active part of Centennial college community, I was interviewed to share my experience as a web Developer working for WIMTACH.`;
+  aLink.href =
+    "https://wimtach.centennialcollege.ca/centennial-college-graduate-rediscovers-his-passion-for-it/";
+  h4Text.innerText = "";
+  pText.classList.remove("italic");
+  pText.innerText = `As an active part of the Centennial College community, I was interviewed to share my experience as a web developer working for WIMTACH.`;
 });
 
-srcImage5.addEventListener('click', () => {
-  targetImage.src = './assets/images/personal/5.jpg';
+srcImage5.addEventListener("click", () => {
+  targetImage.src = "./assets/images/personal/5.jpg";
 
-  h1Text.innerText = 'Hobies / Entrepreneur';
-  h3Text.innerText = '';  
-  aLink.innerText = 'Link to my store';
+  h1Text.innerText = "Hobbies / Entrepreneur";
+  h3Text.innerText = "";
+  aLink.innerText = "Link to my store";
   aLink.href = "https://www.etsy.com/ca/shop/nextlevelboardgame";
-  h4Text.innerText = '';
-  pText.classList.remove('italic');
-  pText.innerText = `I love soccer, traveling, board games, 3D printing and watching movies and series.
+  h4Text.innerText = "";
+  pText.classList.remove("italic");
+  pText.innerText = `I love soccer, travelling, board games, 3D printing and watching movies and series.
   
-  As an entrepenuer, I combined two of my hobbies to make money and have fun at the same time. I make board game parts on my 3D printer and sell them on Etsy. `
+  As an entrepreneur, I combined two of my hobbies to make money and have fun simultaneously. I create board game parts on my 3D printer and sell them on Etsy. `;
 });
-
--- scripts/portifolio.js
+++ scripts/portifolio.js
@@ -3,6 +3,7 @@ const modal1 = document.getElementById("my-modal-project-1");
 const modal2 = document.getElementById("my-modal-project-2");
 const modal3 = document.getElementById("my-modal-project-3");
 const modal4 = document.getElementById("my-modal-project-4");
+const modal5 = document.getElementById("my-modal-project-5");
 
 // console.log(modal1);
 // console.log(modal2);
@@ -14,6 +15,7 @@ const modal4 = document.getElementById("my-modal-project-4");
 const btn2 = document.getElementById("project-2-btn");
 const btn3 = document.getElementById("project-3-btn");
 const btn4 = document.getElementById("project-4-btn");
+const btn5 = document.getElementById("project-5-btn");
 
 // console.log(btn1);
 // console.log(btn2);
@@ -25,53 +27,61 @@ const btn4 = document.getElementById("project-4-btn");
 const close2 = document.getElementsByClassName("close")[1];
 const close3 = document.getElementsByClassName("close")[2];
 const close4 = document.getElementsByClassName("close")[3];
+const close5 = document.getElementsByClassName("close")[4];
 
 // When the user clicks on the button, open the modal
 // btn1.onclick = function() {
 //   modal1.style.display = "block";
 // }
-btn2.onclick = function() {
+btn2.onclick = function () {
   modal2.style.display = "block";
-}
-btn3.onclick = function() {
+};
+btn3.onclick = function () {
   modal3.style.display = "block";
-}
-btn4.onclick = function() {
+};
+btn4.onclick = function () {
   modal4.style.display = "block";
-}
-
-
+};
+btn5.onclick = function () {
+  modal5.style.display = "block";
+};
 
 // When the user clicks on <span> (x), close the modal
 // close1.onclick = function() {
 //   modal1.style.display = "none";
 // }
-close2.onclick = function() {
+close2.onclick = function () {
   modal2.style.display = "none";
-}
-close3.onclick = function() {
+};
+close3.onclick = function () {
   modal3.style.display = "none";
-}
-close4.onclick = function() {
+};
+close4.onclick = function () {
   modal4.style.display = "none";
-}
-
+};
+close5.onclick = function () {
+  modal5.style.display = "none";
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal1) {
     modal1.style.display = "none";
   } else {
-      if (event.target == modal2) {
-        modal2.style.display = "none"; 
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    } else {
+      if (event.target == modal3) {
+        modal3.style.display = "none";
       } else {
-          if (event.target == modal3) {
-            modal3.style.display = "none"; 
-          } else {
-              if (event.target == modal4) {
-                modal4.style.display = "none";    
-              } 
+        if (event.target == modal4) {
+          modal4.style.display = "none";
+        } else {
+          if (event.target == modal5) {
+            modal5.style.display = "none";
           }
+        }
       }
     }
-}
+  }
+};
