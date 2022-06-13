ecd1faa34ad1e82c230b1315ad857971051adeee
Theo Dias Mitchell <theomitchell@gmail.com>
2022-06-13 14:22:17 -0400

    23. Commit after cleaning

--- scripts/introduction.js
+++ scripts/introduction.js
@@ -13,57 +13,57 @@ let h4Text = document.querySelector("h4");
 let pText = document.querySelector("p");
 // console.log(aLink);
 
-srcImage2.addEventListener("click", () => {
-  targetImage.src = "./assets/images/personal/2.JPEG";
+// srcImage2.addEventListener("click", () => {
+//   targetImage.src = "./assets/images/personal/2.JPEG";
 
-  h1Text.innerText = "Student and Professional";
-  h3Text.innerText = "Eager to learn and highly motivated.";
-  aLink.innerText = "";
-  aLink.href = "";
-  h4Text.innerText = "";
-  pText.classList.remove("italic");
-  pText.innerText = `Graduated from Centennial College with high honours and Juno College of Technology. My  proven skills, knowledge, and abilities in programming, debugging, and testing web or desktop-based application, makes me an excellent asset to your organization.`;
-  // console.log(aLink);
-});
+//   h1Text.innerText = "Student and Professional";
+//   h3Text.innerText = "Eager to learn and highly motivated.";
+//   aLink.innerText = "";
+//   aLink.href = "";
+//   h4Text.innerText = "";
+//   pText.classList.remove("italic");
+//   pText.innerText = `Graduated from Centennial College with high honours and Juno College of Technology. My  proven skills, knowledge, and abilities in programming, debugging, and testing web or desktop-based application, makes me an excellent asset to your organization.`;
+//   // console.log(aLink);
+// });
 
-srcImage3.addEventListener("click", () => {
-  targetImage.src = "./assets/images/personal/3.jpg";
+// srcImage3.addEventListener("click", () => {
+//   targetImage.src = "./assets/images/personal/3.jpg";
 
-  h1Text.innerText = "Soft Skills";
-  h3Text.innerText = "Writing code to change the world!";
-  aLink.innerText = "";
-  aLink.href = "";
-  h4Text.innerText = "";
-  pText.classList.remove("italic");
-  pText.innerText = `Excellent communicator both in English and Portuguese, reliable, proactive, effective team player, efficient, committed, time and goal-oriented and critical thinker. `;
-});
+//   h1Text.innerText = "Soft Skills";
+//   h3Text.innerText = "Writing code to change the world!";
+//   aLink.innerText = "";
+//   aLink.href = "";
+//   h4Text.innerText = "";
+//   pText.classList.remove("italic");
+//   pText.innerText = `Excellent communicator both in English and Portuguese, reliable, proactive, effective team player, efficient, committed, time and goal-oriented and critical thinker. `;
+// });
 
-srcImage4.addEventListener("click", () => {
-  targetImage.src = "./assets/images/personal/4.jpg";
+// srcImage4.addEventListener("click", () => {
+//   targetImage.src = "./assets/images/personal/4.jpg";
 
-  h1Text.innerText = "Interview";
-  h3Text.innerText = "";
-  aLink.innerText = "Link to Interview";
-  aLink.href =
-    "https://wimtach.centennialcollege.ca/centennial-college-graduate-rediscovers-his-passion-for-it/";
-  h4Text.innerText = "";
-  pText.classList.remove("italic");
-  pText.innerText = `As an active part of the Centennial College community, I was interviewed to share my experience as a web developer working for WIMTACH.`;
-});
+//   h1Text.innerText = "Interview";
+//   h3Text.innerText = "";
+//   aLink.innerText = "Link to Interview";
+//   aLink.href =
+//     "https://wimtach.centennialcollege.ca/centennial-college-graduate-rediscovers-his-passion-for-it/";
+//   h4Text.innerText = "";
+//   pText.classList.remove("italic");
+//   pText.innerText = `As an active part of the Centennial College community, I was interviewed to share my experience as a web developer working for WIMTACH.`;
+// });
 
-srcImage5.addEventListener("click", () => {
-  targetImage.src = "./assets/images/personal/5.jpg";
+// srcImage5.addEventListener("click", () => {
+//   targetImage.src = "./assets/images/personal/5.jpg";
 
-  h1Text.innerText = "Hobbies / Entrepreneur";
-  h3Text.innerText = "";
-  aLink.innerText = "Link to my store";
-  aLink.href = "https://www.etsy.com/ca/shop/nextlevelboardgame";
-  h4Text.innerText = "";
-  pText.classList.remove("italic");
-  pText.innerText = `I love soccer, travelling, board games, 3D printing and watching movies and series.
+//   h1Text.innerText = "Hobbies / Entrepreneur";
+//   h3Text.innerText = "";
+//   aLink.innerText = "Link to my store";
+//   aLink.href = "https://www.etsy.com/ca/shop/nextlevelboardgame";
+//   h4Text.innerText = "";
+//   pText.classList.remove("italic");
+//   pText.innerText = `I love soccer, travelling, board games, 3D printing and watching movies and series.
   
-  As an entrepreneur, I combined two of my hobbies to make money and have fun simultaneously. I create board game parts on my 3D printer and sell them on Etsy. `;
-});
+//   As an entrepreneur, I combined two of my hobbies to make money and have fun simultaneously. I create board game parts on my 3D printer and sell them on Etsy. `;
+// });
 
 
 
@@ -157,13 +157,13 @@ nextButton.addEventListener("click", (e) => {
 });
 
 const slide0 = () => {
-  h1Text.innerText = "Slide 0";
-  h3Text.innerText = "A";
-  aLink.innerText = "A";
-  aLink.href = "A";
-  h4Text.innerText = "A";
-  pText.classList.remove("italic");
-  pText.innerText = `A`;
+  h1Text.innerText = "Hi, I'm Theo Mitchell";
+  h3Text.innerText = "A Web Developer and Software Engineer Technician.";
+  aLink.innerText = "";
+  aLink.href = "";
+  h4Text.innerText = "Coding to change the world!";
+  // pText.classList.remove("italic");
+  pText.innerText = `Click on the arrows to know me a little better.`;
 
 };
 
@@ -205,8 +205,7 @@ const slide4 = () => {
   aLink.href = "https://www.etsy.com/ca/shop/nextlevelboardgame";
   h4Text.innerText = "";
   pText.classList.remove("italic");
-  pText.innerText = `I love soccer, travelling, board games, 3D printing and watching movies and series.
-  
+  pText.innerText = `I love soccer, travelling, board games, 3D printing and watching movies and series.  
   As an entrepreneur, I combined two of my hobbies to make money and have fun simultaneously. I create board game parts on my 3D printer and sell them on Etsy. `;
 };
 
